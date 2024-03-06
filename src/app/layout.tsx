424ca1f87da3dec3f04bc559e80c95b370e22a4a
Evan <93_jm@naver.com>
2024-03-06 15:16:13 +0900

    Update: ogImage 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     description: "Just Evan, 프론트엔드 개발자 블로그, 포트폴리오, 웹사이트",
     images: [
       {
-        url: "/github.png",
+        url: "https://evanlee-dev.com/ogImage.png",
         width: 600,
         height: 400,
       },
